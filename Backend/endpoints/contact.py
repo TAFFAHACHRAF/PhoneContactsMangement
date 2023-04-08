adc6915d50c76efb0ff8f6b86f70a6173539c7ac
Achraf TAFFAH <a.taffah@etu.enset-media.ac.ma>
2023-04-08 22:42:35 +0200

    post and put

--- Backend/endpoints/contact.py
+++ Backend/endpoints/contact.py
@@ -4,6 +4,8 @@ from models.response import Response
 from models.models import Contact
 from db.database import Database
 from sqlalchemy import and_, desc
+from sqlalchemy import Integer
+
 
 # APIRouter creates path operations for product module
 router = APIRouter(
@@ -20,3 +22,24 @@ async def read_all_contacts():
     session = database.get_db_session(engine)
     data = session.query(Contact).all()
     return Response(data, 200, "Contacts retrieved successfully.", False)
+
+@router.post("/")
+async def add_contact(surname: str, lastname: str, email: str, picture:str):
+    contact = Contact(surname, lastname, email, picture)
+    session = database.get_db_session(engine)
+    session.add(contact)
+    session.commit()
+    return Response(contact,200,"Contact added successfully", False)
+
+@router.put("/{id}")
+async def put_contact(id: int, surname: str, lastname: str, email: str, picture:str):
+    session = database.get_db_session(engine)
+    contact=Contact(id,surname,lastname,email,picture)
+    contact_to_update = session.query(Contact).filter(Contact.id == id).first()
+
+    contact_to_update.surname = surname
+    contact_to_update.lastname = lastname
+    contact_to_update.email = email
+    contact_to_update.picture = picture
+    session.commit()
+    return Response(True, 200, "Contact updated successfully", False)
--- Backend/models/models.py
+++ Backend/models/models.py
@@ -2,12 +2,18 @@ from sqlalchemy.orm import declarative_base
 from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
-
 class Contact(Base):
-    __tablename__ = "CONTACT"
-
+    __tablename__ = 'CONTACT'
     CONTACT_ID = Column(Integer, primary_key=True, index=True)
-    CONTACT_SURNAME = Column(String)
-    CONTACT_LASTNAME = Column(String)
-    CONTACT_EMAIL = Column(String)
-    CONTACT_PICTURE = Column(String)
+    CONTACT_SURNAME = Column(String, nullable=False)
+    CONTACT_LASTNAME = Column(String, nullable=False)
+    CONTACT_EMAIL = Column(String, unique=True, nullable=False)
+    CONTACT_PICTURE = Column(String, nullable=False)
+
+    def __init__(self, CONTACT_SURNAME: str, CONTACT_LASTNAME: str, CONTACT_EMAIL: str, CONTACT_PICTURE:str):
+        self.CONTACT_ID=0
+        self.CONTACT_SURNAME = CONTACT_SURNAME
+        self.CONTACT_LASTNAME = CONTACT_LASTNAME
+        self.CONTACT_EMAIL = CONTACT_EMAIL
+        self.CONTACT_PICTURE = CONTACT_PICTURE  
+
